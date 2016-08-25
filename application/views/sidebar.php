d166c6ca5db7280c5b7ed686d265ccca85cb2940
YazidAsykurillah <asykurillahy@gmail.com>
2016-08-26 03:18:22 +0700

    remove unnecessary links

--- application/views/sidebar.php
+++ application/views/sidebar.php
@@ -59,14 +59,14 @@
 				<li>
 					<a><i class="fa fa-cog"></i> Manajemen DJK <span class="fa fa-chevron-down"></span></a>
 					<ul class="nav child_menu" style="display: none">
-						<li><?php echo anchor('djk_management/', 'Manajemen DJK'); ?></li>
+						<!-- <li><?php echo anchor('djk_management/', 'Manajemen DJK'); ?></li> -->
 						<li><?php echo anchor('djk_management/certification', 'Acara Sertifikasi'); ?></li>
 					</ul>
 				</li>
 				<li>
 					<a><i class="fa fa-cog"></i> Galeri <span class="fa fa-chevron-down"></span></a>
 					<ul class="nav child_menu" style="display: none">
-						<li><?php echo anchor('gallery/', 'Galeri'); ?></li>
+						<!-- <li><?php echo anchor('gallery/', 'Galeri'); ?></li> -->
 						<li><?php echo anchor('gallery/album', 'Album'); ?></li>
 					</ul>
 				</li>
